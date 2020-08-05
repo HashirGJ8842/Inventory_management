da22b9b103048cea2a3bae22776a18f8c8e595ef
Hashir Hameed Niazi <hameedhashirniazi@gmail.com>
2020-08-05 22:06:34 +0530

    Added email for notifying

--- src/components/singleNotification.vue
+++ src/components/singleNotification.vue
@@ -4,7 +4,8 @@
           <div class="card-header">{{ getCardHeader(notification.type) }}</div>
             <div class="card-body">
             <p class="card-text">{{ notification.body }}</p>
-            <v-btn @click="agreedClickHandler()">Approve</v-btn>
+            <v-btn @click="agreedClickHandler(notification.email)">Approve</v-btn>
+            <v-btn @click="rejectedClickHandler(notification.email)">Reject</v-btn>
             </div>
             <div class="card-footer">{{ notification.name }}</div>
       </div>
@@ -50,7 +51,7 @@ export default {
         {
             return `Notification for :- ${type.charAt(0).toUpperCase() + type.slice(1)}`
         },
-        agreedClickHandler()
+        agreedClickHandler(email)
         {
             db.ref('notification').push({
             Name: this.$store.state.user.displayName,
@@ -58,6 +59,30 @@ export default {
             Body: `${this.$store.state.user.displayName} has agreed to the booking.`,
             Type: 'info-agreed'
           })
+        window.Email.send({
+            SecureToken : "516b8123-d120-42df-9bc2-de17edd0f342",
+            To : email,
+            From : "abc@example.com",
+            Subject : "You can issue your item, your request was accepted",
+            Body : "<html><h2>Header</h2></html>"
+        })
+        this.$router.push({name: 'JC'})
+        },
+        rejectedClickHandler(email)
+        {
+            db.ref('notification').push({
+            Name: this.$store.state.user.displayName,
+            Email: this.$store.state.user.email,
+            Body: `${this.$store.state.user.displayName} has rejected to the booking.`,
+            Type: 'info-rejected'
+          })
+        window.Email.send({
+            SecureToken : "516b8123-d120-42df-9bc2-de17edd0f342",
+            To : email,
+            From : "abc@example.com",
+            Subject : "Sorry, You can't issue your item right now.",
+            Body : "<html><h2>Request rejected</h2></html>"
+        })
           this.$router.push({name: 'JC'})
         }
     }
